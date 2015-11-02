6bfc7c5b4d987e698658585eed73178aaf3411c8
Perry Kivolowitz <perry@kivolowitz.com>
2015-11-02 11:20:54 -0600

    Added subroutines and texture support

--- per-fragment-phong.fs.glsl
+++ per-fragment-phong.fs.glsl
@@ -1,7 +1,11 @@
 #version 410 core
+//#extension GL_ARB_explicit_uniform_location : enable
 
 // Output
 layout (location = 0) out vec4 FragColor;
+
+subroutine vec4 color_t();
+subroutine uniform color_t ColorMode;
 
 // Input from vertex shader - this must correspond exactly
 // to the outputs of the vertex shader. The semantics of
@@ -22,29 +26,60 @@ uniform vec3 specular_albedo;
 uniform float specular_power;
 uniform vec3 ambient;
 
-// Position of light is taken to be in eye coordinates.
-uniform vec3 light_position = vec3(0.0, 0.0, 10.0);
+// Default position of light is taken to be in eye coordinates.
+uniform vec3 light_position = vec3(0.0, 0.0, 1000.0);
 
-void main(void)
+uniform sampler2D base_texture;
+
+subroutine(color_t)
+vec4 Constant()
+{
+	return vec4(ambient, 1.0);
+}
+
+subroutine(color_t)
+vec4 PerPixelLighting()
 {
 	vec3 diffuse2 = diffuse_albedo;
 	vec3 N2 = fs_in.N;
 
 	if (!gl_FrontFacing)
 	{
-		//FragColor = vec4(ambient, 1.0);
 		diffuse2 = vec3(fs_in.C);
 		N2 = -N2;
 	}
-	//else
-	//{
-		vec3 n = normalize(N2);
-		vec3 s = normalize(light_position - fs_in.P);
-		vec3 v = normalize(-fs_in.P);
-		vec3 r = reflect(-s, n);
-		vec3 diffuse = max(dot(s, n), 0.0) * diffuse2;
-		vec3 specular = pow(max(dot(r, v), 0.0), specular_power) * specular_albedo;
+	vec3 n = normalize(N2);
+	vec3 s = normalize(light_position - fs_in.P);
+	vec3 v = normalize(-fs_in.P);
+	vec3 r = reflect(-s, n);
+	vec3 diffuse = max(dot(s, n), 0.0) * diffuse2;
+	vec3 specular = pow(max(dot(r, v), 0.0), specular_power) * specular_albedo;
 
-		FragColor = vec4(ambient + diffuse + specular, 1.0);
-	//}
+	return vec4(ambient + diffuse + specular, 1.0);
+}
+
+subroutine(color_t)
+vec4 PPLWithTexture()
+{
+	vec3 diffuse2 = vec3(texture(base_texture, fs_in.T));
+	vec3 N2 = fs_in.N;
+
+	if (!gl_FrontFacing)
+	{
+		diffuse2 = vec3(fs_in.C);
+		N2 = -N2;
+	}
+	vec3 n = normalize(N2);
+	vec3 s = normalize(light_position - fs_in.P);
+	vec3 v = normalize(-fs_in.P);
+	vec3 r = reflect(-s, n);
+	vec3 diffuse = max(dot(s, n), 0.0) * diffuse2;
+	vec3 specular = pow(max(dot(r, v), 0.0), specular_power) * specular_albedo;
+
+	return vec4(ambient + diffuse + specular, 1.0);
+}
+
+void main(void)
+{
+	FragColor = ColorMode();
 }
